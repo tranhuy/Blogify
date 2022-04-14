d7679b7b6591be5e321016840135d9a69db687d0
Huy <huy.tran@hotmail.com>
2022-04-14 10:33:06 -0400

    added self-ping to prevent Heroku dyno from sleeping

--- api/index.js
+++ api/index.js
@@ -7,4 +7,10 @@ const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
+    
+    // issues ping to server to prevent Heroku dynos from sleeping
+    setInterval(() => {
+        console.log(`Pinging ${process.env.APP_URL}`);
+        http.get(process.env.APP_URL);   
+    }, 600000);
 })
