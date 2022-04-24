4e4fd6dc294f48577d30f7fcc79d5e00e0f4ff53
Huy <huy.tran@hotmail.com>
2022-04-24 00:17:11 -0400

    removed auto ping server code

--- api/index.js
+++ api/index.js
@@ -9,8 +9,8 @@ server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
     
     // issues ping to server to prevent Heroku dynos from sleeping
-    setInterval(() => {
-        console.log(`Pinging ${process.env.APP_URL}`);
-        http.get(process.env.APP_URL);   
-    }, 600000);
+    // setInterval(() => {
+    //     console.log(`Pinging ${process.env.APP_URL}`);
+    //     http.get(process.env.APP_URL);   
+    // }, 600000);
 })
